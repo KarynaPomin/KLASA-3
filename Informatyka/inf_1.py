8fce83eb8acac9b86fc838bc497a6f4a47dc8385
pk22 <115844791+KarynaPomin@users.noreply.github.com>
2024-09-04 15:06:29 +0200

    Create inf_1.py

---
+++ Informatyka/inf_1.py
@@ -0,0 +1,36 @@
+def czy_podzielna_przez_2(n):
+    return n % 2 == 0
+
+n = int(input("Podaj: "))
+if czy_podzielna_przez_2(n):
+    print("Tak")
+else:
+    print("Nie")
+
+def dzielniki_1(n):
+    for i in range(1, n + 1):
+        if n % i == 0:
+            print(i, end=" ")
+    print()
+
+def dzielniki_2(n): # pierwiasten(n) - 1
+    i = 1
+    while i * i < n:
+        if n % i == 0:
+            print(int(i), end=" ")
+            print(int(n / i), end=" ")
+        i = i + 1
+    if i * i == n:
+        print(int(i), end=" ")
+    print()
+dzielniki_2(n)
+
+def suma_dzielnikow(n):
+    suma = 0
+    for i in range(1, n + 1):
+        if n % i == 0:
+            suma += i
+    print(suma)
+suma_dzielnikow(n)
+
+def 
