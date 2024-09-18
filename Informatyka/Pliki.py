46f959ca47b0b73b9121c9499e3866838b6a5870
pk22 <115844791+KarynaPomin@users.noreply.github.com>
2024-09-18 15:06:03 +0200

    Update Pliki.py

--- Informatyka/Pliki.py
+++ Informatyka/Pliki.py
@@ -1,8 +1,15 @@
 import math
 
-# wczytanie w piionie
+#ciag = plik.read() # wczyta wszystkie linie do tablicy
+#ciag = plik.readline() # wczyta telko jedną linię do tablicy
+#ciag = plik.readlines() # wczyta wszystkie linie oraz wiersze do tablicy do tablicy
+
 plik = open("liczby1.txt", "r")
-liczby = list(map(int, plik.read().split()))
+liczby_1 = list(map(int, plik.read().split()))
+plik.close()
+
+plik = open("liczby2.txt", "r")
+liczby_2 = list(map(int, plik.read().split()))
 plik.close()
 
 def CzyPierwsza(n):
@@ -21,9 +28,34 @@ def Zad_1():
             ile += 1
     print(ile)
 
+def liczbaDzielnikowWlasciwych(n):
+    ile = 0
+    for i in range(1, n):
+        if n % i == 0:
+            ile += 1
+    return ile
 
-#ciag = plik.read() # wczyta wszystkie linie do tablicy
-#ciag = plik.readline() # wczyta telko jedną linię do tablicy
-#ciag = plik.readlines() # wczyta wszystkie linie oraz wiersze do tablicy do tablicy
+def Zad_3():
+    suma = 0
+    for liczba in liczby_2:
+        suma += liczbaDzielnikowWlasciwych(liczba)
+    print(suma)
 
-# wczytanie w poziomie
+def sumaCzynnikowPierwszych(n):
+    czynnik = 2
+    suma = 0
+    while n > 1:
+        while n % czynnik == 0:
+            suma += czynnik
+            n //= czynnik
+        czynnik += 1
+    return suma
+
+def Zad_5():
+    sumaCzynnikow = []
+    for liczba in liczby_2:
+        sumaCzynnikow.append(sumaCzynnikowPierwszych(liczba))
+    minimum = min(sumaCzynnikow)
+    for i in range(len(liczby_2)):
+        if sumaCzynnikow[i] == minimum:
+            print(liczby_2[i])
