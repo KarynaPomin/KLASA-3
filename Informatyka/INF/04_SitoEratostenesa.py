451703c9404b50f6fb361b1ac9f3ccdc1f6049ad
pk22 <115844791+KarynaPomin@users.noreply.github.com>
2024-10-01 11:23:25 +0200

    Update 04_SitoEratostenesa.py

--- Informatyka/INF/04_SitoEratostenesa.py
+++ Informatyka/INF/04_SitoEratostenesa.py
@@ -55,3 +55,35 @@ def WhileSitoEratostenesa(n):
             print(i, end=" ")
 
 # Zad 4
+def CzyPierwszaSitoEratostenesa(n):
+    czyPierwszaList = [True] * (n + 1)
+    czyPierwszaList[0] = False
+    czyPierwszaList[1] = False
+
+    p = 2
+    while p * p <= n:
+        if czyPierwszaList[p]:
+            for i in range(p * p, n + 1, p):
+                czyPierwszaList[i] = False
+        p = p + 1
+
+    for i in range(2, n + 1):
+        if czyPierwszaList[i] == True:
+            return True
+
+# Nie zakończone
+def Zad_4():
+    SitoEratostenesa(1000)
+
+    plik = open("liczby.txt", "r")
+    LiczbyList = list(map(int, plik.read().split()))
+    plik.close()
+
+    ileLiczbPierwszych = 0
+    for liczba in LiczbyList:
+        if CzyPierwszaSitoEratostenesa(liczba):
+            ileLiczbPierwszych += 1
+    print(ileLiczbPierwszych)
+    # Odp. 1735
+
+Zad_4()
