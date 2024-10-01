b1441ce3431b1d24a341ac33b89a5e77230ab1d6
pk22 <115844791+KarynaPomin@users.noreply.github.com>
2024-10-01 11:07:14 +0200

    Update 04_SitoEratostenesa.py

--- Informatyka/INF/04_SitoEratostenesa.py
+++ Informatyka/INF/04_SitoEratostenesa.py
@@ -16,3 +16,42 @@ def SitoEratostenesa(n):
             print(i, end=" ")
 
 # Zad 2
+def SumaSitoEratostenesa(n):
+    czyPierwszaList = [True] * (n + 1)
+    czyPierwszaList[0] = False
+    czyPierwszaList[1] = False
+
+    sumaDzielnikow = 0
+    p = 2
+    while p * p <= n:
+        if czyPierwszaList[p]:
+            for i in range(p * p, n + 1, p):
+                czyPierwszaList[i] = False
+        p = p + 1
+
+    for i in range(2, n + 1):
+        if czyPierwszaList[i] == True:
+            sumaDzielnikow += i
+
+    print(n, sumaDzielnikow)
+
+# Zad 3
+def WhileSitoEratostenesa(n):
+    czyPierwszaList = [True] * (n + 1)
+    czyPierwszaList[0] = False
+    czyPierwszaList[1] = False
+
+    p = 2
+    while p * p <= n:
+        if czyPierwszaList[p]:
+            i = p * p
+            while i < n + 1:
+                czyPierwszaList[i] = False
+                i += p
+        p = p + 1
+
+    for i in range(2, n + 1):
+        if czyPierwszaList[i] == True:
+            print(i, end=" ")
+
+# Zad 4
